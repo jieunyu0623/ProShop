69ad3de597ee294ffe7ecb7bcb5ea3f7e270490a
jieunyu0623 <55225394+jieunyu0623@users.noreply.github.com>
2022-01-11 23:54:18 -0800

    backend api endpoints setup and database setup

---
+++ backend/data/users.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Jieun Yu',
+    email: 'jieun@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jeongho Han',
+    email: 'jeongho@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+];
+
+export default users;
